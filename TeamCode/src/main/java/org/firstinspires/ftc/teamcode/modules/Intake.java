d85eedcb34985082d879acce63adbc8ad3866ca0
DarkCrow1020 <rcapornita@gmail.com>
2026-03-26 15:44:04 +0200

    Update code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Intake.java
@@ -8,7 +8,7 @@ public class Intake {
     private DcMotor intake1,intake2;
     private Servo intakeServo1,intakeServo2;
     private int direction = 0;
-    private float servoPosition= 0;
+    private float servoPosition1= 0.4f ,servoPosition2=0.1f;
 
     public Intake(HardwareMap hardwareMap) {
         intake1 = hardwareMap.get(DcMotor.class, "intake1");
@@ -18,12 +18,13 @@ public class Intake {
     }
     public void forward() { direction = 1; }
     public void reverse() { direction = -1; }
-    public void servoDown(){servoPosition=0.1f;}
-    public void servoUp(){servoPosition=0.4f;}
+    public void servoDown(){servoPosition1=0.1f;servoPosition2=0.4f;}
+    public void servoUp(){servoPosition1=0.4f;servoPosition2=0.1f;}
     public void stop()    { direction = 0; }
     public void update() {
         intake1.setPower(direction);
         intake2.setPower(-direction);
-        intakeServo1.setPosition(servoPosition);
+        intakeServo1.setPosition(servoPosition1);
+        intakeServo2.setPosition(servoPosition2);
     }
 }
