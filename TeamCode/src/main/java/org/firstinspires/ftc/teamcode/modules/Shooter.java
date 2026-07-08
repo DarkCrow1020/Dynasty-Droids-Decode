1c4175c927b8b5a8327bf7838c0e37a091aacafb
DarkCrow1020 <rcapornita@gmail.com>
2026-07-08 13:25:00 +0300

    Added cycle to shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
@@ -7,16 +7,18 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 public class Shooter {
-    private DcMotor shooter1, shooter2;
-    private Servo servoAngle,stopper;
+    private DcMotor shooter1, shooter2,intake1,intake2;
+    private Servo servoAngle1,stopper;
     private boolean shooterOn = false;
     private ElapsedTime timer = new ElapsedTime();
-
+    private int cycle;
     public Shooter(HardwareMap hardwareMap) {
         shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
         shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+        intake1 = hardwareMap.get(DcMotor.class, "intake1");
+        intake2 = hardwareMap.get(DcMotor.class, "intake2");
 
-        servoAngle = hardwareMap.get(Servo.class, "servoAngle");
+        servoAngle1 = hardwareMap.get(Servo.class, "servoAngle1");
         stopper = hardwareMap.get(Servo.class,"stopper");
 
 
@@ -35,17 +37,46 @@ public class Shooter {
         if (shooterOn) {
             shooter1.setPower(-1);
             shooter2.setPower(1);
-            if (timer.milliseconds() < 1500) {
-                stopper.setPosition(0.25);
-            } else{
-                stopper.setPosition(0);
+            if(cycle==0){
+                timer.reset();
+                cycle=1;
             }
+            if(timer.milliseconds()<1500) {
+            intake1.setPower(0);
+            intake2.setPower(0);
+            return;
+            }
+            if(timer.milliseconds()<2200){
+            intake1.setPower(1);
+            intake2.setPower(1);
+            return;
+            }
+            cycle++;
+            timer.reset();
+
+            if(cycle>3){
+                shooterOn=false;
+                intake1.setPower(0);
+                intake2.setPower(0);
+                shooter2.setPower(0);
+                shooter1.setPower(0);
+                cycle=0;
+            }
+
         } else {
             shooter1.setPower(0);
             shooter2.setPower(0);
-            stopper.setPosition(0.25);
+            intake2.setPower(0);
+            intake1.setPower(0);
+            cycle=0;
         }
     }
+
     public boolean isOn() { return shooterOn; }
+    public void stopper(){
+        stopper.setPosition(0);
+    }
+
 
 }
+
